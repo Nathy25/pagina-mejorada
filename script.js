358cb7e4476f0c7a582b45d2d5f49498cc13c07c
Nathy <118781345+Nathy25@users.noreply.github.com>
2023-08-01 23:58:14 -0300

    noticias con script

---
+++ script.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+    $("#comentForm").submit(function (event) {
+        event.preventDefault();
+
+        const firsName = $("#firsName").val();
+        const email = $("#email").val();
+        const coments = $("#coments").val();
+
+        const newComment = `<li><strong>${firsName}</strong> (${email}):<br>${coments}</li>`;
+
+        $("#coment").append(newComment);
+
+        $("#comentForm")[0].reset();
+
+    });
+});
+
+
+$(document).ready(function() {
+    $(".btn").click((evt) => {
+        alert($(evt.target).html())
+    })
+
+})
+
+
